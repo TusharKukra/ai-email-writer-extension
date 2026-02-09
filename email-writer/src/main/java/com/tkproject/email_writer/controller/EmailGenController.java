2fd101be966783d94b8cce61aff4b5fad9a0085d
tusharkukra <tusharkukra15@gmail.com>
2026-02-09 18:35:47 +0530

    added config for aws secrets manager

--- email-writer/src/main/java/com/tkproject/email_writer/controller/EmailGenController.java
+++ email-writer/src/main/java/com/tkproject/email_writer/controller/EmailGenController.java
@@ -3,16 +3,25 @@ package com.tkproject.email_writer.controller;
 import com.tkproject.email_writer.entity.EmailDTO;
 import com.tkproject.email_writer.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/email")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class EmailGenController {
 
     @Autowired
     private EmailService emailService;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @GetMapping("/home/url")
+    public String homePage() {
+        return "Hello from AI Email Reply Generator Spring Boot Application, url: " + geminiApiUrl;
+    }
 
     @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailDTO emailDto) {
